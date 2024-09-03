046c4168df721aace91477b2c828976e10f2e078
Fady <fedo.zaher@gmail.com>
2024-09-03 11:50:20 +0300

    enhance

--- lib/src/presentation/widgets/custom_app_bar_widget.dart
+++ lib/src/presentation/widgets/custom_app_bar_widget.dart
@@ -57,32 +57,31 @@ class CustomAppBarWidget extends StatelessWidget
               //how to add number in routing from city eye
               width: MediaQuery.of(context).size.width * 0.55,
               height: 45,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(6),
-                ),
-                child: TextField(
-                  style: const TextStyle(
-                    color: Colors.grey,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 20,
-                  ),
-                  onSubmitted: (String value) {
-                    context.go('/search/$value');
-                  },
-                  decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.clear),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          context.go('/search/');
-                        },
-                        icon: const Icon(Icons.cancel),
+              child: TextField(
+                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: ColorSchemes.gray,
+                    ),
+                onSubmitted: (String value) {
+                  context.go('/search/$value');
+                },
+                decoration: InputDecoration(
+                    prefixIcon:
+                        const Icon(Icons.clear, color: Color(0xFFE2B616)),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        // context.go('/search/');
+                      },
+                      icon: const Icon(Icons.cancel, color: Color(0xFFE2B616)),
+                    ),
+                    hintText: "Search...",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6),
+                      borderSide: const BorderSide(
+                        color: Color(0xFFE2B616),
                       ),
-                      hintText: "Search...",
-                      border: InputBorder.none),
-                ),
+                    )),
               ),
             )
           ],
