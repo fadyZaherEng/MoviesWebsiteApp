faa16efa3b6840d49fa68b38986f8da972ef0bb6
Fady <fedo.zaher@gmail.com>
2024-09-08 13:46:01 +0300

    fix and deploy

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -22,6 +22,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "appTitle": MessageLookupByLibrary.simpleMessage("موقع الفيلم"),
         "home": MessageLookupByLibrary.simpleMessage("الصفحة الرئيسية"),
         "movies": MessageLookupByLibrary.simpleMessage("الافلام")
       };
